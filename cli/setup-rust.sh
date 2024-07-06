50423b0126c511e99412ad5ff3880e4504f787c4
Haydon Lam <haydon.lam@starlingbank.com>
2024-07-06 10:43:47 +0100

    revert to rustup stable as we are not using rayon

--- cli/setup-rust.sh
+++ cli/setup-rust.sh
@@ -5,8 +5,13 @@ rustupHomeDir="$projectDir"/.rustup
 mkdir -p $rustupHomeDir
 export RUSTUP_HOME=$rustupHomeDir
 export LIBRARY_PATH=$LIBRARY_PATH:"$projectDir"/nix/profile/default/lib
+
 # nightly version is needed for wasm-bindgen-rayon
-rustup default nightly-2024-06-12
+# rustup install nightly-2024-06-12
+# rustup default nightly-2024-06-12
+
+rustup default stable
+
 # rust-src is needed for -Z build-std=panic_abort,std, as required by wasm-bindgen-rayon
 rustup component add rust-src
 # wasm-pack is needed for building wasm
